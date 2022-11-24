14f59eb70f06756717de361c4ad0075531ea8732
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-24 17:10:27 +0530

    Print Welcome

---
+++ MoodAnalyser_MSTesting/MoodAnalyser_MSTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyser_MSTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
