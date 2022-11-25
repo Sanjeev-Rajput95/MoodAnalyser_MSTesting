ed9719b544232ca3fd073389e5e646a80df067d5
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-25 19:47:32 +0530

    Return Sad

---
+++ MoodAnalyser_MSTesting/MoodAnalyser_MSTesting/MoodAnayser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser_MSTesting
+{
+    public class MoodAnayser
+    {
+        public string AnalyseMood(string message)
+        {
+           // string messeage = "I am in Sad Mood";
+            bool result = message.Contains("Sad");
+            if (result)
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "";
+            }
+        } 
+    }
+}
--- MoodAnalyser_MSTesting/MoodAnalyser_MSTesting/Program.cs
+++ MoodAnalyser_MSTesting/MoodAnalyser_MSTesting/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace MoodAnalyser_MSTesting
 {
-    internal class Program
+  public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("************* WELCOME TO MOODANALYSER MSTESTING  ********************");
+
+            Console.WriteLine("Enter string to check Mood ");
+           // string mood = Console.ReadLine();
+            MoodAnayser moodAnayser = new MoodAnayser();
+
+            MoodAnayser obj = new MoodAnayser();
+            string res = obj.AnalyseMood("I am in Sad Mood");
+            Console.WriteLine(res);
         }
+
     }
 }
