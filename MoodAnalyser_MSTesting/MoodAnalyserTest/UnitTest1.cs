076f998f9660f972892d1cfcbc24df4f0445f5e8
sanjeev <sanjeevsingh.77d@gmail.com>
2022-12-02 00:47:51 +0530

    Reflection Given Class Name When Improper Should Throw MoodAnalysisException

--- MoodAnalyser_MSTesting/MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyser_MSTesting/MoodAnalyserTest/UnitTest1.cs
@@ -28,5 +28,52 @@ namespace MoodAnalyserTest
             string result = moodAnayser.AnalyseMood();
             Assert.AreEqual("HAPPY".ToUpper(), result);
         }
+
+        [TestMethod]
+        public void TestExceptionHandling()
+        {
+            MoodAnayser moodAnayser = new MoodAnayser(null);
+            string result = moodAnayser.AnalyseMood();
+            Assert.AreEqual("Should Not NULL value", result);
+        }
+
+        [TestMethod]
+        public void TestExceptionReturn_HAPPY_WhenPassNull()
+        {
+            MoodAnayser moodAnayser = new MoodAnayser(null);
+            string result = moodAnayser.AnalyseMood();
+            Assert.AreEqual("It is Null", result);
+        }
+
+        [TestMethod]
+        public void CustomException_when_Null()
+        {
+            MoodAnayser moodAnayser = new MoodAnayser(null);
+            string result = moodAnayser.AnalyseMood();
+            Assert.AreEqual("It is Null", result);
+        }
+        [TestMethod]
+        public void CustomException_when_Empty()
+        {
+            MoodAnayser moodAnayser = new MoodAnayser("");
+            string result = moodAnayser.AnalyseMood();
+            Assert.AreEqual("It is a empty string", result);
+        }
+
+        [TestMethod]
+        public void Reflection_GivenClassName_Return_Object()
+        {
+            MoodAnayser moodAnayser = new MoodAnayser();
+            var obj = MoodAnayserFactory.CreateInstance("MoodAnalyser_MSTesting.MoodAnayser");
+            Assert.IsInstanceOfType(moodAnayser, (Type)obj);
+        }
+
+        [TestMethod]
+        public void Reflection_GivenInvalideClass_Return_NoSuchClass_Exception()
+        {
+            //MoodAnayser moodAnayser = new MoodAnayser();
+            var obj = MoodAnayserFactory.CreateInstance("MoodAnalyser_MSTesting.MoodAnayse");
+            Assert.AreEqual("No such class", obj);
+        }
     }
 }
